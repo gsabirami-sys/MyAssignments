05491b1373ee468521beee00a2127d5f5c1286ce
gsabirami-sys <gsabirami@gmail.com>
2025-12-07 07:26:38 +0530

    Add files via upload

---
+++ day1/day1/EmployeeDetailsAssignment.java
@@ -0,0 +1,23 @@
+
+
+public class EmployeeDetailsAssignment {
+	
+	public static void main(String args[])
+	{
+		
+		String name="Abirami";
+		int age =30;
+		double salary=55000.75;
+		
+		char gender = 'F';
+		boolean acceptTerms = true;
+		double rating = 4.5f;
+		String mobileNumber = "9876543210";
+			int	pinCode = 56001;
+			int luckyNumber = 7;
+
+
+		System.out.println(name+"my name is\n"+age+"my age is\n"+salary+"\n"+"my gender is \n"+gender+"\n"+acceptTerms+"\n"+rating+"my rating is \n"+mobileNumber+"my pincode is \n"+pinCode+"\n"+luckyNumber+"my lucky number is \n");
+	}
+
+}
---
+++ day1/day1/FirefoxWithVariables.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class FirefoxWithVariables {
+	public static void main(String args[])
+	{
+double browserVersion=100.2;
+String name="firefox";
+boolean isvisible=true;
+short releaseYear=1998;
+char browserlogo='f';
+
+
+System.out.println(browserVersion+"\n"+name+"\n"+isvisible+"\n"+releaseYear+"\n"+browserlogo);
+
+
+}
+}
