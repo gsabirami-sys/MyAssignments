48f1e6d68667d59fc5c39a02e861c11caa404dbe
gsabirami-sys <gsabirami@gmail.com>
2025-12-07 07:55:04 +0530

    Add files via upload

---
+++ day1/EmployeeDetailsAssignment.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class EmployeeDetailsAssignment {
+	
+	public static void main(String args[])
+	{
+		
+		String name="Abirami";
+		int age =30;
+		double salary=55000.75;
+		
+		char gender = 'F';
+		boolean acceptTerms = true;
+		double rating = 4.5f;
+		long mobileNumber = 9876543210L;
+			int	pinCode = 56001;
+			byte luckyNumber = 7;
+
+
+		System.out.println("my name is"+" "+name+"\n"+"my age is"+" "+age+"\n"+"salary:"+" "+salary+"\n"+"my gender is "+gender+"\n"+acceptTerms+"\n"+"my rating is "+rating+"\n"+"Mobile number:"+mobileNumber+"\n"+"my pincode is"+" "+""+pinCode+"\n"+"my lucky number is:"+""+luckyNumber);
+	}
+
+}
